20ba0578d2c604ba29977d2e67085a28b0b4c0a0
ewanreis <87541930+ewanreis@users.noreply.github.com>
2021-12-06 15:29:01 +0000

    update

---
+++ Assets/Scripts/Female.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Female : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if( Input.GetKey("w") )
+        {
+            anim.SetBool("walk", true);    
+        }
+        else
+        {
+            anim.SetBool("walk", false);
+        }
+
+
+    }
+}
